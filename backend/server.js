837095d0d79d8494fe5b5d4d0602d1998072f9c6
Azhar-khann <azhar.uddinn.work@gmail.com>
2023-10-11 16:58:08 +0500

    for render cookie

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
     secret: "D53gxl41G",
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 300000000, secure: true, domain: 'localhost', path: '/' },
+    cookie: { maxAge: 300000000, secure: true, sameSite: "none" },
     store,
   })
 );
