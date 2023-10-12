745af8613ea162324134dfd021513a59cd555e1d
Azhar-khann <azhar.uddinn.work@gmail.com>
2023-10-12 17:01:52 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
     secret: "D53gxl41G",
     resave: false,
     saveUninitialized: false,
-    cookie: { httpOnly: true, secure: true, maxAge: 1000 * 60 * 60 * 48, sameSite: 'none'},
+    cookie: { maxAge: 1000 * 60 * 60 * 48, secure: true},
     store,
   })
 );
